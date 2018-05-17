f62477e2198cdff2d2def5b2ef724585ff912287
BrandonSersion <SersionSoftware@protonmail.com>
2018-05-16 23:46:42 -0400

    Add API view post

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from .serializers import HelloSerializer
 
 # Create your views here.
 
 
 class HelloApiView(APIView):
     """Test API View."""
+
+    serializer_class = HelloSerializer
     
     def get(self,request, format=None):
         """Returns a list of APIView features."""
@@ -20,3 +25,16 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name."""
+
+        serializer = HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
